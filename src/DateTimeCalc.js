0fec115041f1d0060056c2d96a24acedefede5b2
Zach Askins <zach303@msn.com>
2017-05-25 14:55:35 -0700

    radio buttons and simple layout

--- src/DateTimeCalc.js
+++ src/DateTimeCalc.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import DateTimeFields from './DateTimeFields';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 
 class DateTimeCalc extends Component {
   render() {
     return (
       <div>
         <h1>Add or Subtract Moments in Time</h1>
-        <DateTimeFields />
+        <form>
+          <div className="fields">
+            <DateTimeFields />
+            <RadioButtonGroup>
+              <RadioButton
+                value="add"
+                label="Add"
+              />
+              <RadioButton
+                value="subtract"
+                label="Subtract"
+              />
+            </RadioButtonGroup>
+            <DateTimeFields />
+          </div>
+        </form>
       </div>
       
     )
