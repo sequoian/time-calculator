f97319e3dde06b38df97602c64ad3c99afb1c84c
Zach Askins <zach303@msn.com>
2017-05-25 19:36:02 -0700

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
-//import DateTimeCalculator from './DateTimeCalc';
-import TimeCalcuator from './TimeCalc';
+import DateTimeCalculator from './DateTimeCalc';
+//import TimeCalcuator from './TimeCalc';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import {List, ListItem} from 'material-ui/List';
 
 injectTapEventPlugin();
+
+class SideBar extends Component {
+  render() {
+    return (
+      <List className="side-bar">
+        <ListItem primaryText="Add or Subtract Datetimes" />
+        <ListItem primaryText="Add or Subtract Lengths of Time" />
+      </List>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MuiThemeProvider>
-          <TimeCalcuator />
+          <div className="main">
+            <SideBar />
+            <div className="calculator">
+              <DateTimeCalculator />
+            </div>
+          </div>
         </MuiThemeProvider>
       </div>
     );
