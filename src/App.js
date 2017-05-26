b06874547691d443cf59b20cf5888bb7be70fd54
Zach Askins <zach303@msn.com>
2017-05-26 12:23:58 -0700

    state for changing calculators

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import DateTimeCalculator from './DateTimeCalc';
-//import TimeCalcuator from './TimeCalc';
+import TimeCalcuator from './TimeCalc';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {List, ListItem} from 'material-ui/List';
 
 injectTapEventPlugin();
+
+const PAGES = {
+  DATETIMECALC: 'dt+-dt',
+  TIMECALC: 'ln+-ln'
+}
 
 class SideBar extends Component {
   render() {
@@ -20,14 +25,28 @@ class SideBar extends Component {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: PAGES.DATETIMECALC
+    }
+  }
   render() {
+    let calculator = null;
+    if (this.state.currentPage === PAGES.DATETIMECALC) {
+      calculator = <DateTimeCalculator />
+    }
+    else if (this.state.currentPage === PAGES.TIMECALC) {
+      calculator = <TimeCalcuator />
+    }
+
     return (
       <div className="App">
         <MuiThemeProvider>
           <div className="main">
             <SideBar />
             <div className="calculator">
-              <DateTimeCalculator />
+              {calculator}
             </div>
           </div>
         </MuiThemeProvider>
