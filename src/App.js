6145d466cbf7e849a00df90ef01bce05b776c497
Zach Askins <zach303@msn.com>
2017-05-27 12:51:38 -0700

    sidebar shows selected calculator

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import DateTimeCalculator from './DateTimeCalc';
 import TimeCalcuator from './TimeCalc';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import {List, ListItem} from 'material-ui/List';
+import {List, ListItem, makeSelectable} from 'material-ui/List';
+import PropTypes from 'prop-types';
 
 injectTapEventPlugin();
 
@@ -13,19 +14,56 @@ const PAGES = {
   TIMECALC: 'ln+-ln'
 }
 
+// create a higher order component that allows material-ui list to be selectable
+let SelectableList = makeSelectable(List);
+function wrapState(ComposedComponent) {
+  return class SelectableList extends Component {
+    static propTypes = {
+      children: PropTypes.node.isRequired,
+      defaultValue: PropTypes.number.isRequired
+    };
+
+    componentWillMount() {
+      this.setState({
+        selectedIndex: this.props.defaultValue
+      });
+    }
+
+    handleChange = (event, index) => {
+      this.setState({
+        selectedIndex: index
+      });
+    }
+
+    render() {
+      return (
+        <ComposedComponent
+          value={this.state.selectedIndex}
+          onChange={this.handleChange}
+        >
+          {this.props.children}
+        </ComposedComponent>
+      );
+    }
+  }
+}
+SelectableList = wrapState(SelectableList);
+
 class SideBar extends Component {
   render() {
     return (
-      <List className="side-bar">
+      <SelectableList defaultValue={1} className="side-bar">
         <ListItem 
           primaryText="Difference Between Two Datetimes"
           onClick={() => this.props.changePage(PAGES.DATETIMECALC)}
+          value={1}
         />
         <ListItem 
           primaryText="Add or Subtract Lengths of Time" 
           onClick={() => this.props.changePage(PAGES.TIMECALC)}
+          value={2}
         />
-      </List>
+      </SelectableList>
     );
   }
 }
